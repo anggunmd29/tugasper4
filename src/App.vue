1bd1b8a38709fdbe4afbe1cf4d2de13b35c662e4
anggunmd29 <anggunmustikadewi@student.uir.ac.id>
2025-03-16 23:41:49 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import tugas4 from './components/tugas4.vue';
+</script>
+
+<template>
+<tugas4/>
+</template>
+
---
+++ src/components/tugas4.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+const showMessage = ref(true);
+const message = ref('Pesan Awal');
+const textColor = ref('black');
+
+function increment() {
+  count.value++;
+  if (count.value > 5) {
+    showMessage.value = false;
+    message.value = 'Pesan setelah count > 5';
+    textColor.value = 'red';
+  }
+}
+
+function changeMessage() {
+  message.value = 'Pesan telah diubah!';
+  textColor.value = 'blue';
+}
+</script>
+
+<template>
+  <div>
+    <button @click="increment">Tambah Count ({{ count }})</button>
+    <button @click="changeMessage">Ubah Pesan</button>
+    <h1 :style="{ color: textColor }" v-if="showMessage">{{ message }}</h1>
+    <p v-else>Pesan disembunyikan!</p>
+  </div>
+</template>
+
